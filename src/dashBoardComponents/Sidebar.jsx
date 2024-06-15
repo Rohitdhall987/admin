feaa9a496ffa4a00dc5c43346517d078aca01bfc
Rohitdhall987 <rohitdhall987@gmail.com>
2024-06-15 21:15:52 +0530

    modified:   src/dashBoardComponents/Sidebar.jsx 	modified:   src/index.css

--- src/dashBoardComponents/Sidebar.jsx
+++ src/dashBoardComponents/Sidebar.jsx
@@ -3,9 +3,11 @@ import { Link } from "react-router-dom";
 function Sidebar() {
     return (
       <div className="col-span-2 flex-col flex card">
-        <Link className="text-white" to="/dashboard/home">home</Link>
-        <Link className="text-white" to="/dashboard/playlist">Play List</Link>
-        <Link className="text-white" to="/dashboard/add">3</Link>
+        <Link className="menu-item" to="/dashboard/home"><span className="material-icons" >music_note</span><p className="pl-1">Songs</p></Link>
+        <hr/>
+        <Link className="menu-item" to="/dashboard/playlist"><span className="material-icons" >queue_music</span><p className="pl-1">Playlist</p></Link>
+        <hr/>
+        <Link className="menu-item" to="/dashboard/add"><span className="material-icons" >group</span><p className="pl-1">Users</p ></Link>
       </div>
     );
   }
