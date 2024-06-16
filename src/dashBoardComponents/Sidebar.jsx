1a4fd6c52b9d41e85dd6e97be0dace3aa5085c0f
Rohitdhall987 <rohitdhall987@gmail.com>
2024-06-16 18:36:38 +0530

    modified:   src/dashBoardComponents/Sidebar.jsx 	modified:   src/index.css

--- src/dashBoardComponents/Sidebar.jsx
+++ src/dashBoardComponents/Sidebar.jsx
@@ -1,13 +1,21 @@
 import { Link } from "react-router-dom";
 
 function Sidebar() {
+  const titleClass="pl-1 hidden md:block";
     return (
       <div className="col-span-2 flex-col flex card">
-        <Link className="menu-item" to="/dashboard/home"><span className="material-icons" >music_note</span><p className="pl-1">Songs</p></Link>
+        <Link className="menu-item" to="/dashboard/home"><span className="material-icons">music_note</span><p className={titleClass}>Songs</p></Link>
         <hr/>
-        <Link className="menu-item" to="/dashboard/playlist"><span className="material-icons" >queue_music</span><p className="pl-1">Playlist</p></Link>
+        <Link className="menu-item" to="/dashboard/playlist"><span className="material-icons">queue_music</span><p className={titleClass}>Playlist</p></Link>
         <hr/>
-        <Link className="menu-item" to="/dashboard/add"><span className="material-icons" >group</span><p className="pl-1">Users</p ></Link>
+        <Link className="menu-item" to="/dashboard/categories"><span className="material-icons">category</span><p className={titleClass}>Categories</p></Link>
+        <hr/>
+        <Link className="menu-item" to="/dashboard/albums"><span className="material-icons">album</span><p className={titleClass}>Albums</p></Link>
+        <hr/>
+        <Link className="menu-item" to="/dashboard/artists"><span className="material-icons">person</span><p className={titleClass}>Artists</p></Link>
+        <hr/>
+        <Link className="menu-item" to="/dashboard/add"><span className="material-icons">group</span><p className={titleClass}>Users</p></Link>
+
       </div>
     );
   }
