50ba2ca17f5ce4053b2ec7c7b259f97b989c18e2
Rohitdhall987 <rohitdhall987@gmail.com>
2024-06-20 20:46:44 +0530

    modified:   src/dashBoardComponents/Sidebar.jsx 	modified:   src/index.css

--- src/dashBoardComponents/Sidebar.jsx
+++ src/dashBoardComponents/Sidebar.jsx
@@ -1,8 +1,7 @@
-import { useState } from "react";
+
 import { Link, useLocation } from "react-router-dom";
 
 function Sidebar() {
-  const [selectedItem, setSelectedItem] = useState("/dashboard/songs"); // Default selected item
   const location = useLocation();
 
   // Function to determine if a link is selected
@@ -15,62 +14,56 @@ function Sidebar() {
       <Link
         className={`menu-item ${isSelected('/dashboard/songs') ? 'selected' : ''}`}
         to="/dashboard/songs"
-        onClick={() => setSelectedItem('/dashboard/songs')}
       >
         <span className="material-icons">music_note</span>
         <p className={titleClass}>Songs</p>
       </Link>
-      {isSelected('/dashboard/songs') && <hr />}
+      {isSelected('/dashboard/songs') && <hr className="animated" />}
 
       <Link
         className={`menu-item ${isSelected('/dashboard/playlist') ? 'selected' : ''}`}
         to="/dashboard/playlist"
-        onClick={() => setSelectedItem('/dashboard/playlist')}
       >
         <span className="material-icons">queue_music</span>
         <p className={titleClass}>Playlist</p>
       </Link>
-      {isSelected('/dashboard/playlist') && <hr />}
+      {isSelected('/dashboard/playlist') && <hr className="animated" />}
 
       <Link
         className={`menu-item ${isSelected('/dashboard/categories') ? 'selected' : ''}`}
         to="/dashboard/categories"
-        onClick={() => setSelectedItem('/dashboard/categories')}
       >
         <span className="material-icons">category</span>
         <p className={titleClass}>Categories</p>
       </Link>
-      {isSelected('/dashboard/categories') && <hr />}
+      {isSelected('/dashboard/categories') && <hr className="animated" />}
 
       <Link
         className={`menu-item ${isSelected('/dashboard/albums') ? 'selected' : ''}`}
         to="/dashboard/albums"
-        onClick={() => setSelectedItem('/dashboard/albums')}
       >
         <span className="material-icons">album</span>
         <p className={titleClass}>Albums</p>
       </Link>
-      {isSelected('/dashboard/albums') && <hr />}
+      {isSelected('/dashboard/albums') && <hr className="animated" />}
 
       <Link
         className={`menu-item ${isSelected('/dashboard/artists') ? 'selected' : ''}`}
         to="/dashboard/artists"
-        onClick={() => setSelectedItem('/dashboard/artists')}
       >
         <span className="material-icons">person</span>
         <p className={titleClass}>Artists</p>
       </Link>
-      {isSelected('/dashboard/artists') && <hr />}
+      {isSelected('/dashboard/artists') && <hr className="animated" />}
 
       <Link
         className={`menu-item ${isSelected('/dashboard/add') ? 'selected' : ''}`}
         to="/dashboard/add"
-        onClick={() => setSelectedItem('/dashboard/add')}
       >
         <span className="material-icons">group</span>
         <p className={titleClass}>Users</p>
       </Link>
-      {isSelected('/dashboard/add') && <hr />}
+      {isSelected('/dashboard/add') && <hr className="animated" />}
     </div>
   );
 }
